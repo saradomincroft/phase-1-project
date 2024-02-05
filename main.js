90718c60b4f72ea3d9b709fe18fb8e46cc46582a
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-05 19:50:34 +1100

    changed html city to dropdown state option, added basic js to start

---
+++ main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginPage = document.getElementById("login-page");
+    const eventSearchPage = document.getElementById("event-search-page");
+    // const eventResultsDiv = document.getElementById('eventResults');
+  
+    const mockUser = {
+      username: "testUser",
+      password: "testPassword"
+    };
+  
+    window.login = function() {
+      const usernameInput = document.getElementById("username");
+      const passwordInput = document.getElementById("password");
+      const errorMessage = document.getElementById("login-error");
+      
+      if (
+        usernameInput.value === mockUser.username &&
+        passwordInput.value === mockUser.password
+      ) {
+        loginPage.style.display = "none";
+        eventSearchPage.style.display = "block";
+        errorMessage.style.display = "none";
+  
+  
+      } else {
+        errorMessage.style.display = "block";
+      }
+    };
+  
+  
+  });
+  
