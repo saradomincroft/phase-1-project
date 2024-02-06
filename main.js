1f8d889df6e72ef26064c1702fca6018f497a6b8
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-06 21:55:30 +1100

    added keys to secret file

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
+// import keys from 'ignore' keys file for privacy
+import { eventbriteApiKey, ticketmasterApiKey } from './keys.js';
+
 document.addEventListener('DOMContentLoaded', function() {
     const loginPage = document.getElementById("login-page");
     const eventSearchPage = document.getElementById("event-search-page");
-    // const eventResultsDiv = document.getElementById('eventResults');
+    const eventResultsDiv = document.getElementById('search-results');
   
     const mockUser = {
       username: "testUser",
       password: "testPassword"
     };
-  
+
     window.login = function() {
       const usernameInput = document.getElementById("username");
       const passwordInput = document.getElementById("password");
